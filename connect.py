09a4bc73271dfbcbb5f855c8c8d9bea34bb1010e
Geulmaster <eyal.geulayev@gmail.com>
2020-10-06 23:30:03 +0300

    Allow post function send json

--- connect.py
+++ connect.py
@@ -16,13 +16,13 @@ def get(url, route: str, prms: dict = None):
         logger.info(f"Successfully got {route} params: " + str(prms) if prms else '')
         return res.content
 
-def post(url, route: str, expected_status=201):
+def post(url, route: str, data: dict = None):
     try:
-        res = requests.post(url + route, timeout=connection_timeout)
+        res = requests.post(url + route, data, timeout=connection_timeout)
     except Exception as e:
         logger.fatal("Error getting " + route)
         logger.info(str(e))
-    if res.status_code != expected_status:
+    if res.status_code >= 400:
         logger.fatal(f"Error getting {route} from server {str(res.status_code)}")
     else:
         logger.info(f"post {route} - {res.status_code}")
